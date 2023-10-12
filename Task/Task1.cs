d75f8898d5200f441e00af4076d3fe7af6652d25
Daulet <daulet@qit.kz>
2023-10-12 22:26:28 +0600

    Add project files.

---
+++ Task/Task1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class GeometryCalculator
+{
+    public static double CalculateCircleArea(double radius)
+    {
+        if (radius <= 0)
+            throw new ArgumentException("раадиус должен быть положительным числом.");
+        return Math.PI * Math.Pow(radius, 2);
+    }
+
+    public static double CalculateTriangleArea(double a, double b, double c)
+    {
+        if (a <= 0 || b <= 0 || c <= 0)
+            throw new ArgumentException("длины сторон треугольника должны быть положительными числами.");
+        if (a + b <= c || a + c <= b || b + c <= a)
+            throw new ArgumentException("треугольник с такими сторонами не существует.");
+        double s = (a + b + c) / 2;
+        return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    public static bool IsRightTriangle(double a, double b, double c)
+    {
+        var sides = new double[] { a, b, c };
+        Array.Sort(sides);
+        return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
+    }
+}
+
---
+++ Task/task2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+}
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+}
+
+public class ProductCategory
+{
+    public int ProductId { get; set; }
+    public int CategoryId { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Product> products = new List<Product>
+        {
+            new Product { ProductId = 1, ProductName = "Product A" },
+            new Product { ProductId = 2, ProductName = "Product B" },
+            new Product { ProductId = 3, ProductName = "Product C" }
+        };
+
+        List<Category> categories = new List<Category>
+        {
+            new Category { CategoryId = 1, CategoryName = "Category X" },
+            new Category { CategoryId = 2, CategoryName = "Category Y" }
+        };
+
+        List<ProductCategory> productCategories = new List<ProductCategory>
+        {
+            new ProductCategory { ProductId = 1, CategoryId = 1 },
+            new ProductCategory { ProductId = 2, CategoryId = 2 },
+            new ProductCategory { ProductId = 3, CategoryId = 1 },
+        };
+
+        var query = from product in products
+                    join pc in productCategories on product.ProductId equals pc.ProductId into gj
+                    from subCategory in gj.DefaultIfEmpty()
+                    join category in categories on subCategory?.CategoryId ?? -1 equals category.CategoryId into gj2
+                    from subCategory2 in gj2.DefaultIfEmpty()
+                    select new
+                    {
+                        ProductName = product.ProductName,
+                        CategoryName = subCategory2?.CategoryName
+                    };
+
+        foreach (var result in query)
+        {
+            Console.WriteLine($"Product: {result.ProductName}, Category: {result.CategoryName ?? "No Category"}");
+        }
+    }
+}
+
